381d14419c9ad8d3eb05950accd68751798c2519
chrisriecen-pixel <chrisriecen@gmail.com>
2026-01-11 21:46:57 +0200

    fix: complete Task 1 with WelcomeMessage component

---
+++ alx-react-app/src/components/WelcomeMessage.jsx
@@ -0,0 +1,10 @@
+function WelcomeMessage() {
+  return (
+    <>
+      <h1>Welcome to ALX React</h1>
+      <p>This is your first React component</p>
+    </>
+  );
+}
+
+export default WelcomeMessage;
