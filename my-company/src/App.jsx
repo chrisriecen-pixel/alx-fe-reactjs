9ea5383287263ba43d1632ea6aa43a115108f0c2
chrisriecen-pixel <chrisriecen@gmail.com>
2026-01-18 22:32:48 +0200

    feat: complete company website with routing and contact form for Task 3

---
+++ my-company/src/App.jsx
@@ -0,0 +1,16 @@
+import Footer from './Footer';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  );
+}
