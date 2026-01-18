d5fcfb83721df742ae028985c5614dbbd2d4eb37
chrisriecen-pixel <chrisriecen@gmail.com>
2026-01-18 22:44:39 +0200

    feat: complete company website with routing and contact form for Task 3

--- my-company/src/App.jsx
+++ my-company/src/App.jsx
@@ -1,4 +1,9 @@
-import Footer from './Footer';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './Home';
+import About from './About';
+import Services from './Services';
+import Contact from './Contact';
+import Navbar from './Navbar';
 
 function App() {
   return (
@@ -10,7 +15,8 @@ function App() {
         <Route path="/services" element={<Services />} />
         <Route path="/contact" element={<Contact />} />
       </Routes>
-      <Footer />
     </BrowserRouter>
   );
 }
+
+export default App;
