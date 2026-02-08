2b45389f80668052969341119eb496a5aa8d9d32
chrisriecen-pixel <chrisriecen@gmail.com>
2026-02-08 23:38:23 +0200

    Task 0: Correct Vite and Tailwind configs for validator

--- tailwind-react-integration/vite.config.js
+++ tailwind-react-integration/vite.config.js
@@ -1,10 +1,6 @@
-module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+})
